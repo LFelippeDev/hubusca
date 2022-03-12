2ee5ab00c0d3e181aa708e56fe06df39e73b7576
LFelippDev <lfelippeeng@gmail.com>
2022-03-11 22:39:00 -0300

    Cut not used components

--- src/app/styles/SeparatioLine.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const SeparationLine = styled.div`
-  width: 0.15rem;
-  border-radius: 1rem;
-  background: ${(props) => props.theme.colors.primaryDark};
-`;
